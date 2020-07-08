d84e550801316078e40b99cae9762a1fa2c8efd4
Carsten Dominik <carsten.dominik@gmail.com>
2020-07-08 08:30:48 +0200

    Additional examples in the manual

--- misc/selftest.pl
+++ misc/selftest.pl
@@ -9,43 +9,43 @@ my $tests = [
    prepare => "rm -f $dir/dustkapscatmat.dat",
    cmd => "./optool -q -na 10 -nl 30 -s -o $dir",
    getsha => "shasum $dir/dustkapscatmat.dat",
-   sha => 'c944a1e62a56a067c238e18381d55665a8bc66a0'},
+   sha => '4e3efc333829c437e5559eb369175873bdcc4d74'},
 
   {name => 'full-diana-lowres',
    prepare => "rm -f $dir/dustkapscatmat.dat",
    cmd  => "./optool -q -s -nl 30 -o $dir",
    getsha => "shasum $dir/dustkapscatmat.dat",
-   sha => '7d9742c802eba2b0980a7964d9c6f49193f1671c'},
+   sha => 'bff70e1517e0d1152814aec51043a475e67ed831'},
 
   {name => 'diana-plus-ice-mantle',
    prepare => "rm -f $dir/dustkapscatmat.dat",
    cmd  => "./optool -q -c pyr-mg70 0.87 -c c-z 0.13 -m ice-w 0.2 -p 0.25 -s -nl 30 -na 20 -o $dir",
    getsha => "shasum $dir/dustkapscatmat.dat",
-   sha => 'e85676b23afec0e11ac1532395027648e78dfd85'},
+   sha => '60e6a624193712b646cfe127bbd5256dc53d70d3'},
 
   {name => 'radmc-in-10-parts',
    prepare => "rm -f $dir/dust*",
    cmd  => "./optool -q -na 10 -nl 30 -s -d 3 -radmc -o $dir",
    getsha => "cat $dir/dustkapscatmat_*.inp|shasum",
-   sha => 'f8a07c888a900684f19f31da147e645b3ceec43e'},
+   sha => '544efd379fc48e6c677b0ecc68481c4246784f6a'},
 
   {name => 'high-angular-resolution',
    prepare => "rm -f $dir/dust*",
    cmd  => "./optool -q -nl 10 -na 20 -s 720 -o $dir",
    getsha => "shasum $dir/dustkapscatmat.dat",
-   sha => 'a9c679270d8116639eef3fdbfb118c64c34a892c'},
+   sha => 'c7369ec904e593b0b0161605034e2d6e938d070d'},
 
   {name => 'pure-ice-grain',
    prepare => "rm -f $dir/dust*",
    cmd  => "./optool -q -c ice-w 1.0 -a 1 3 2.5 15 -l 10 100 -s -o $dir",
    getsha => "shasum $dir/dustkapscatmat.dat",
-   sha => '9e9dfc0acaaaeab7d8c8e5fcd26873e18bb621f9'},
+   sha => '8ac91d9a1d1240f9c10de65ae6acae431831c3df'},
 
   {name => 'chop-peak',
    prepare => "rm -f $dir/dustkapscatmat.dat",
    cmd => "./optool -q -na 10 -nl 30 -s -chop 4 -o $dir",
    getsha => "shasum $dir/dustkapscatmat.dat",
-   sha => '36f3f949857d85bebbeb8542d3ba292a4fbc3352'},
+   sha => '5111f6e346b34d70a900a59dcc58bbd1b40397a8'},
 
   {name => 'fits-output',
    prepare => "rm -f $dir/dustkappa.fits",
@@ -64,10 +64,13 @@ foreach $test (@$tests) {
   $sha = $1 if $sha =~ /([0-9a-fA-F]+)/;
   if ($sha eq $test->{sha}) {
     $result = "Test $test->{name} passed.\n";
-    $r = sprintf "Test %-30s passed",$test->{name}
+    $r = sprintf "Test %-30s passed",$test->{name};
+    # system('say Great!')
   } else {
     $result = "Test $test->{name} failed: resulting SHA1 $sha does not match\n";
-    $r = sprintf "Test %-30s FAILED %s",$test->{name},$sha
+    $r = sprintf "Test %-30s FAILED %s",$test->{name},$sha;
+    # make selftest
+    system('say Ooops!')
   }
   print "$result";
   push @results,$r;
