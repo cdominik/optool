3f40f4e3cf9ff220d2b6f85b51318f7dad156a26
Carsten Dominik <carsten.dominik@gmail.com>
2020-07-15 17:52:52 +0200

    Fixed the ifort selftests

--- misc/selftest.pl
+++ misc/selftest.pl
@@ -9,54 +9,61 @@ getopts('s');
 $dir = 'selftest_optool';
 
 my $tests = [
+  {name => 'noscat',
+   prepare => "rm -f $dir/dustkapscatmat.dat",
+   cmd => "./optool -q -na 10 -nl 30 -o $dir",
+   getsha => "accuracy",
+   glob => "$dir/dustkappa.dat",
+   sha => '1000:f06d9f2404::10:f06d9f2404::9:f06d9f2404::8:f06d9f2404::7:f06d9f2404::6:f06d9f2404::5:49cc8c16ec::4:9904743c57::3:ce63507d3d::2:78f49a2bea::1:365bce6134'},
+
   {name => 'quick',
    prepare => "rm -f $dir/dustkapscatmat.dat",
    cmd => "./optool -q -na 10 -nl 30 -s -o $dir",
    getsha => "accuracy",
    glob => "$dir/dustkapscatmat.dat",
-   sha => '1000:08c1e5e93e::10:08c1e5e93e::9:08c1e5e93e::8:08c1e5e93e::7:08c1e5e93e::6:08c1e5e93e::5:90f3452d60::4:e4ca4b1256::3:08fc12c7b9::2:d4712e9d2a::1:a448e211f8'},
+   sha => '1000:6aa496e97e::10:6aa496e97e::9:6aa496e97e::8:6aa496e97e::7:6aa496e97e::6:6aa496e97e::5:df9d530594::4:05a29985ab::3:3d11f253ca::2:c65dd298a9::1:925d32f42f'},
 
   {name => 'full-diana-lowres',
    prepare => "rm -f $dir/dustkapscatmat.dat",
    cmd  => "./optool -q -s -nl 30 -o $dir",
    getsha => "accuracy",
    glob => "$dir/dustkapscatmat.dat",
-   sha => '1000:da886965eb::10:da886965eb::9:da886965eb::8:da886965eb::7:da886965eb::6:da886965eb::5:46e523a4b5::4:5bfdada8c5::3:189ccc4ada::2:26b3ca4e4e::1:f39a6c5ce8'},
+   sha => '1000:74ca4f8633::10:74ca4f8633::9:74ca4f8633::8:74ca4f8633::7:74ca4f8633::6:74ca4f8633::5:a7d555142b::4:77ae4e872d::3:e47d05bc1b::2:d83386d92d::1:32a313008d'},
 
   {name => 'diana-plus-ice-mantle',
    prepare => "rm -f $dir/dustkapscatmat.dat",
    cmd  => "./optool -q -c pyr-mg70 0.87 -c c-z 0.13 -m ice-w 0.2 -p 0.25 -s -nl 30 -na 20 -o $dir",
    getsha => "accuracy",
    glob => "$dir/dustkapscatmat.dat",
-   sha => '1000:4767575447::10:4767575447::9:4767575447::8:4767575447::7:4767575447::6:4767575447::5:38c6b004b3::4:c08c36c0f0::3:ce8b9ccc27::2:cbbee6c5b3::1:222eba0515'},
+   sha => '1000:4b5ad766f6::10:4b5ad766f6::9:4b5ad766f6::8:4b5ad766f6::7:4b5ad766f6::6:4b5ad766f6::5:cc5323374c::4:1085a0ab32::3:3894b1ed1d::2:82873cae79::1:c721f5b885'},
 
   {name => 'div-in-10-parts',
    prepare => "rm -f $dir/dust*",
    cmd  => "./optool -q -na 10 -nl 30 -s -d 3 -o $dir",
    getsha => "accuracy",
    glob => "$dir/dustkapscatmat_*.dat",
-   sha => '1000:166fb6e527::10:166fb6e527::9:166fb6e527::8:166fb6e527::7:166fb6e527::6:166fb6e527::5:06535b616b::4:3f33db8f72::3:2050818458::2:c7a33f2081::1:260fbf285d'},
+   sha => '1000:d7e3667bdd::10:d7e3667bdd::9:d7e3667bdd::8:d7e3667bdd::7:d7e3667bdd::6:d7e3667bdd::5:6d93280e93::4:3a5307a850::3:0c6de40511::2:2de07b6969::1:af18ced59c'},
 
   {name => 'high-angular-resolution',
    prepare => "rm -f $dir/dust*",
    cmd  => "./optool -q -nl 10 -na 20 -s 720 -o $dir",
    getsha => "accuracy",
    glob => "$dir/dustkapscatmat.dat",
-   sha => '1000:08e101eb54::10:08e101eb54::9:08e101eb54::8:08e101eb54::7:08e101eb54::6:08e101eb54::5:039187374a::4:bfe7c3c3d8::3:a7f234083f::2:bb7b8f7f57::1:4f405c379d'},
+   sha => '1000:9e6fac720f::10:9e6fac720f::9:9e6fac720f::8:9e6fac720f::7:9e6fac720f::6:9e6fac720f::5:ceff6417e5::4:1b4693158a::3:1e831cbcbb::2:b4f99c5a57::1:11d6de5140'},
 
   {name => 'pure-ice-grain',
    prepare => "rm -f $dir/dust*",
    cmd  => "./optool -q -c ice-w 1.0 -a 1 3 2.5 15 -l 10 100 -s -o $dir",
    getsha => "accuracy",
    glob => "$dir/dustkapscatmat.dat",
-   sha => '1000:37c201f217::10:37c201f217::9:37c201f217::8:37c201f217::7:37c201f217::6:37c201f217::5:7d23baf6cf::4:538f62eb8b::3:406e91c719::2:c92727fd2c::1:8f7afd10b2'},
+   sha => '1000:9d768ddd51::10:9d768ddd51::9:9d768ddd51::8:9d768ddd51::7:9d768ddd51::6:9d768ddd51::5:9319eb7f4c::4:47164fba5a::3:ea72a30411::2:b8eb51996c::1:988f53a934'},
 
   {name => 'chop-peak',
    prepare => "rm -f $dir/dustkapscatmat.dat",
    cmd => "./optool -q -na 10 -nl 30 -s -chop 4 -o $dir",
    getsha => "accuracy",
    glob => "$dir/dustkapscatmat.dat",
-   sha => '1000:1cdd2dcfc3::10:1cdd2dcfc3::9:1cdd2dcfc3::8:1cdd2dcfc3::7:1cdd2dcfc3::6:1cdd2dcfc3::5:d050dcf4ca::4:08325a421f::3:8464dd6cb9::2:051759e079::1:0a1be351e8'},
+   sha => '1000:2e98cf29a7::10:2e98cf29a7::9:2e98cf29a7::8:2e98cf29a7::7:2e98cf29a7::6:2e98cf29a7::5:b7e05c33b6::4:ce86f22c58::3:823189bd80::2:4ef7548ef8::1:ade4724516'},
 
   {name => 'fits-output',
    prepare => "rm -f $dir/dustkappa.fits",
@@ -66,7 +73,7 @@ my $tests = [
   ];
 
 foreach $test (@$tests) {
-  #next unless $test->{name} eq debug;
+  #next unless $test->{name} eq "noscat";
   print "\nRunning test: $test->{name}....\n";
   system($test->{prepare}) if $test->{prepare};
   system($test->{cmd});
@@ -136,10 +143,9 @@ sub accuracy_hashes {
     # print "Doing file $file\n";
     open $fh, '<', $file or die "Can't open file $!";
     $content = do { local $/; <$fh> };
-    print "c1",$content if $debug;
-    $content =~ s/^\s*$//mg; $content =~ s/\n+/\n/mg; # remove empty lines
-    print "c2",$content if $debug;
-    $content =~ s/\s\s+/ /g; # turn multiple whitespace into single space
+    $content =~ s/[\n\t ]+/ /g; # turn multiple whitespace into single space
+    $content =~ s/\s+$//;
+    $content =~ s/^\s+//;
     print "c3",$content if $debug;
     push @all,$content;
   }
