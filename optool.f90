edeb57dc4ad9588fbf26136366de3d70e55e4dfd
Carsten Dominik <dominik@client-145-100-227-148.flexnet2.rug.nl>
2023-03-28 16:26:21 +0200

    Change version numbers and add basic descriptions.

--- optool.f90
+++ optool.f90
@@ -27,7 +27,7 @@ subroutine usage()
   write(stdo,'("")')
   write(stdo,'("===============================================================================")')
   write(stdo,'("optool - dust opacities from the command line")')
-  write(stdo,'("         Dominik, Min, Tazaki 2021, https://ascl.net/2104.010, version 1.9.10")')
+  write(stdo,'("         Dominik, Min, Tazaki 2021, https://ascl.net/2104.010, version 1.9.11")')
   write(stdo,'("")')
   write(stdo,'("-c                        List available materials")')
   write(stdo,'("-c KEY-or-FILE [Mfrac]    Add material with mass fraction. -c may be omitted")')
@@ -264,7 +264,7 @@ program optool
         call manual('all'); stop
 
      case('-version')
-        write(stdo,*) "OpTool version 1.9.10, January 2023, (c) C. Dominik, M. Min & R. Tazaki"
+        write(stdo,*) "OpTool version 1.9.11, April 2023, (c) C. Dominik, M. Min & R. Tazaki"
         stop
 
         ! ----------------------------------------------------------------------
